a9d2b1fc1a2dad0f44367674158316181d8adaa5
hv07webplanex <harsh.webplanex@gmail.com>
2023-09-19 17:25:29 +0530

    Solved a bug in forgot pwd method.

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ app.use(sanitizer.clean({
 // Rate limiting
 const limiter = rateLimit({
 	windowMs: 10 * 60 * 1000, // 10 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes)
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 	// store: ... , // Use an external store for more precise rate limiting
